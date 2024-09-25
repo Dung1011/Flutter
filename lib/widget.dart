4e33f36dddae360545b885c0a88576e3c6fd0ef2
TienDung <nguyendungatsm@gmail.com>
2024-09-24 20:36:45 -0700

    first

---
+++ lib/widget.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+void main(List<String> args) {
+ runApp(const App());
+}
+
+class MyWidget extends StatelessWidget {
+  const MyWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter demo',
+      home: Container(
+        color: Colors.white,
+        child: Center()
+        child: T
+      )
+    );
+  }
+}
